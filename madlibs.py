7f94dd5121bfa914211cdb3da10115a0b901c6e5
luckeyme74 <jmurphy1364@mchenry.edu>
2016-09-01 22:23:53 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+# variables
+noun1 = "lunchbox"
+noun2 = "crayon"
+noun3 = "earring"
+noun4 = "wigwam"
+noun5 = "coffee"
+noun6 = "turtle"
+state1 = "Arizona"
+state2 = "Utah "
+adjective1 = "bashful "
+adjective2 = "crabby "
+girl = "Cher"
+boy = "Sonny"
+
+# song
+print("This " + noun1 + " is your " + noun1 + ",")
+print("This " + noun2 + " is your " + noun2 + ",")
+print("From " + state1 + " to the " + state2 + noun3)
+print("From the " + adjective1 + noun4 + ",")
+print("To the " + adjective2 + noun5 + ",")
+print("This " + noun6 + " was made for " + boy + " and " + girl + ".")
